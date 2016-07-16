1f54200b4151e0bc97f1fec34740bc635376a3b6
Egan McComb <egan.mccomb@gmail.com>
2016-07-16 10:10:25 -0700

    Version dated 5-18-14.

--- freetime.c
+++ freetime.c
@@ -3,17 +3,21 @@
 /*********************************************************/
 /******************** EME154 UCDAVIS *********************/
 /*********************************************************/
-# include <8052.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <MORPH.h>
-# define MAXL 6
+#include <8052.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <MORPH.h>
+
+#define MAXL 6
+#define REVPULSES 8192
+#define DIALTICKS 40
 
 /***** FUNCTION PROTOTYPES *******************************/
 void OCSfunction();
 void MOSfunction();
 void ACSfunction();
 void MSDfunction();
+unsigned char currentDial();
 
 /*********************************************************/
 /***** START OF FTS **************************************/
@@ -23,7 +27,7 @@ void MSDfunction();
   FTS - Freetime System
   INZ - Initialization
   DIG - Diagnostics
-  ERT - Error Treatment Supervisor
+  ERH - Error Treatment Supervisor
   MSS - Machine Status Scan Supervisor
   MCS - Mode Control Supervisor
   ACS - Automatic Control Supervisor
@@ -60,14 +64,14 @@ void printHeader() {
 void printMSD(unsigned char submode) {
 	setCur(0,8);
 	/* Update current initial position. */
-	printf("Current Initial Position: %d\t\t\n", initp);
+	printf("Current Initial Position: %u\t\t\n", initp);
 	if (submode == 0) {
 		/* Display mode. */
 		printf("Press Enter to Change\n");
 	} else {
 		/* Edit mode. */
 		clrLine();
-		printf("Enter Initial Value: \n");
+		printf("Enter Initial Value: ");
 	}
 }
 
@@ -82,7 +86,7 @@ void printACSidle(unsigned char submode) {
 		case 1:
 			setCur(0,9);
 			clrLine();
-			printf("Enter 1st Dial Value: \n");
+			printf("Enter 1st Dial Value: ");
 			break;
 		case 2:
 			setCur(0,10);
@@ -91,7 +95,7 @@ void printACSidle(unsigned char submode) {
 		case 3:
 			setCur(0,10);
 			clrLine();
-			printf("Enter 2nd Dial Value: \n");
+			printf("Enter 2nd Dial Value: ");
 			break;
 		case 4:
 			setCur(0,11);
@@ -100,7 +104,7 @@ void printACSidle(unsigned char submode) {
 		case 5:
 			setCur(0,11);
 			clrLine();
-			printf("Enter 3rd Dial Value: \n");
+			printf("Enter 3rd Dial Value: ");
 			break;
 		case 6:
 			setCur(0,12);
@@ -142,7 +146,7 @@ void printMOS() {
 	if (submode == 0) {
 		/* Display commands. */
 		setCur(0,8);
-		printf("Current Position: XXX\t\t\n");
+		printf("Current Position: %u\t\t\n", currentDial());
 		printf("Q/W        10 ticks CCW/CW\n");
 		printf("A/S         5 ticks CCW/CW\n");
 		printf("Z/X         1 ticks CCW/CW\n");
@@ -151,16 +155,16 @@ void printMOS() {
 	} else {
 		/* Update position. */
 		setCur(0,8);
-		printf("Current Position: XXX\t\t\n");
+		printf("Current Position: %u\t\t\n", currentDial());
 	}
 }
 
 void printMenu() {
 	setCur(0,8);
 	/* Display commands. */
-	printf("1    Machine Setup Data\n");
-	printf("2    Automatic Operation\n");
-	printf("3    Manual Operation\n");
+	printf("1        Machine Setup Data\n");
+	printf("2        Automatic Operation\n");
+	printf("3        Manual Operation\n");
 }
 /***** END of MESSAGE AND CONSTANTS DEFINITION ***********/
 
@@ -171,39 +175,92 @@ void printMenu() {
 /***** START OF INZ **************************************/
 /* Machine message field. */
 char *machineMessage;
-/* Initial position MSD. */
-unsigned char initp = 0;
+/* Initial dial position MSD. */
+unsigned char initp;
+/* Reference encoder position. */
+int encoderref;
 /* Keep track of HMI state inside of a mode. */
-unsigned char submode = 0;
+unsigned char submode;
+/* MSD Ready status: 0 = not ready. */
+unsigned char ready;
 /* Operational mode, error flag, first run flag. */
 unsigned char OMD, ERR1, FFRA;
 /* Timer values. */
 unsigned char MD, MH, MM, MS;
-/* UD counter from RTS. */
-__data __at (0x21) unsigned volatile char counter;
 
-/* Calculate relative number of pulses to achieve desired position. */
-/* XXX: Implement when ASC details are known. */
-unsigned int gotoPosition(unsigned char position) {
+/* UD counter from TIM. */
+__data __at (0x21) unsigned volatile char counter;
+/* ASC servo speed. XXX: Units/desired value? */
+__data __at (0x52) unsigned int servospeed;
+/* ASC relative distance (pulses). */
+__data __at (0x56) unsigned long servopulses;
+/* ASC direction and go bit. 0th bit is go, 1st bit direction (0 = CW).
+ * Conveniently set by: motionRegister = 1 + 0x10*direction;
+ * Position achievement signaled by 1 in 1st bit after go signal.
+ * Conveniently checked by: motionRegister & 0x10;
+ */
+__data __at (0x21) unsigned char motionRegister;
+
+/* Check user input: if valid store in &setting, otherwise throw error. */
+void checkInput(unsigned char input, unsigned char *setting) {
+	/* XXX: Other conditions? */
+	if (input > 39) {
+		/* Invalid input. */
+		machineMessage = "Error: Invalid input (outside 0--39)";
+		/* Get input again. */
+		submode--;
+	} else {
+		/* Store valid setting. */
+		*setting = input;
+	}
+}
+
+/* Function to return current encoder position. */
+int getABSposition() {
+	__data __at (0x60) unsigned long position;
+	return position;
+}
+
+/* Function to return current dial position. */
+unsigned char currentDial() {
+	int absPosition = getABSposition();
+	int relPosition = absPosition - encoderref;
+	unsigned char dial;
+	dial = (initp + relPosition*DIALTICKS/REVPULSES) % DIALTICKS;
+	return dial;
+}
+
+/* Convert dial ticks into encoder pulses. */
+unsigned int ticks2pulses(unsigned char ticks) {
 	unsigned int pulses;
+	pulses = ticks*REVPULSES/DIALTICKS;
 	return pulses;
 }
 
 void INZfunction() {
 	/* Print output to the MPS console. */
 	printMode = 0;
-	/* Operation Mode is Idle. */
+	/* Operation Mode is Idle, default submode */
 	OMD = 0;
+	submode = 0;
 	/* Level 1 error flag for the system. */
 	ERR1 = 0;
-	/* Set First Run Flag. */
+	/* Set First Run Flag, MSD defaults. */
 	FFRA = 1;
+	ready = 0;
+	initp = 0;
 	/* Initialize the time variables. */
 	MD = MH = MM = MS = 0;
+	/* Initialize encoder reference. */
+	encoderref = getABSposition();
 
-	/* Run the Realtime System. */
+	/* Run the TIM RTS. */
 	__asm
 		lcall 0x9600;
+	__endasm;
+	/* Run the ACS RTS. */
+	__asm
+		lcall 0x9F00;
 	__endasm;
 }
 /***** END OF INZ ****************************************/
@@ -264,6 +321,7 @@ void MSSMSD(char selection) {
 			break;
 		case 0x03:	// ^C.
 			/* Return to menu. */
+			encoderref = getABSposition();
 			OMD = 0;
 			break;
 		default:
@@ -373,25 +431,22 @@ void ACSfunction() {
 	machineMessage = "Automatic Mode Accepted";
 
 	/* Count time properly. */
-	if (ACMH > 23) {
-		ACMD++;
-		ACMH = 0;
+	if (MH > 23) {
+		MD++;
+		MH = 0;
 	}
-	if (ACMM > 59) {
-		ACMH++;
-		ACMM = 0;
+	if (MM > 59) {
+		MH++;
+		MM = 0;
 	}
-	if (ACMS > 59) {
-		ACMM++;
-		ACMS = 0;
-	}
-	if (ACMMS > 10) {
-		ACMS++;
+	if (MS > 59) {
+		MM++;
+		MS = 0;
 	}
 	/* 0xFF is the initial. value and 50*20ms is 1s. */
 	if (counter < (0xFF-50)) {
 		/* Prevent counter overflow, count time instead. */
-		ACMS++;
+		MS++;
 		counter = 0xFF;
 	}
 }
@@ -400,7 +455,25 @@ void ACSfunction() {
 
 /***** START OF MSD **************************************/
 void MSDfunction() {
+	/* XXX: Is this dangerous? */
+	char *userinput;
+	unsigned char parsed;
+
 	machineMessage = "Setup Mode Accepted";
+
+	if (submode == 1) {
+		/* Go on to print prompt. */
+		submode++;
+	} else if (submode == 2) {
+		/* Receive user input, parse, and check. */
+		gets(userinput);
+		/* Attempts to cast no matter what:
+		 * there is no detection of obviously erroneous
+		 * input like letters, or negative numbers.
+		 */
+		parsed = atoi(userinput);
+		checkInput(parsed, &initp);
+	}
 }
 /***** END OF MSD ****************************************/
 
